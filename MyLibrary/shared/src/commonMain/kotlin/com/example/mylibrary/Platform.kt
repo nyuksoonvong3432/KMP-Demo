9f957777215621a4ab34f35a6538134a3c4e7eaa
Vong Nyuk Soon <nyuksoon.vong@habitap.app>
2025-06-11 17:38:25 +0800

    clean unused

--- MyLibrary/shared/src/commonMain/kotlin/com/example/mylibrary/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mylibrary
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- MyLibrary/shared/src/iosMain/kotlin/com/example/mylibrary/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mylibrary
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
